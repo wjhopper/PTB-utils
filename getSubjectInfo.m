a35e06c8a9152bb0d1edc88b0ba259e3ee03fee3
whopper-cemnl <wjhopper510@gmail.com>
2015-01-16 14:27:53 -0500

    added callbacks for cancel and run buttons, and actaully parse and return inputted values from getSubjectInfo now

--- getSubjectInfo.m
+++ getSubjectInfo.m
@@ -1,4 +1,4 @@
-function getSubjectInfo(varargin)
+function resps = getSubjectInfo(varargin)
 
 % Calling
 % c=struct('radio',struct('eeg',struct('label','EEG Experiment?')), ...
@@ -36,25 +36,104 @@ fig = dialog('Name',t,'Position',[(dims(3)/2) + width/2, (dims(4)/2) + height/2,
 
 % add elements to fig
 offset= 0;
+
 for i=1:numel(s)
     if any(strcmp(s(i).type, {'textinput','dropdown'}))
-        uicontrol(fig,'Style','text','String',s(i).label, 'Horiz','left', ... 
-            'Position',[margin, height - offset - margin - label_size, width-2*margin, label_size])
-        uicontrol(fig,'Style',table{strcmpi(s(i).type,table(:,1)),2}, 'Horiz','left','Backgr','w', ...
-            'FontSize', 11,  'String', s(i).values, ...
+        uicontrol(fig,'Style','text','String',s(i).label, 'Horiz','left','Tag',s(i).name,  ... 
+            'Position',[margin, height - offset - margin - label_size, width-2*margin, label_size]); %#ok<*AGROW>
+        a = uicontrol(fig,'Style',table{strcmpi(s(i).type,table(:,1)),2}, 'Horiz','left','Backgr','w', ...
+            'FontSize', 11,  'String', s(i).values, 'Tag',s(i).name, ...
             'Position',[margin, height - offset - padding - label_size - TI_size, ...
-                        width-2*margin, TI_size])
+                        width-2*margin, TI_size]);
+        if strcmp(s(i).type, 'textinput')
+            string_objs(i)=a;
+        else
+            drop_objs(i)=a;
+        end
         offset = offset +  50;
     else
-       uicontrol(fig,'Style','checkbox','String',s(i).label,'Val',s(i).values, ... 
-        'Position',[margin, height - offset - margin - CHK_size, width-2*margin, CHK_size])
+       value_objs(i) = uicontrol(fig,'Style','checkbox','String',s(i).label,'Val',s(i).values,'Tag',s(i).name, ... 
+        'Position',[margin, height - offset - margin - CHK_size, width-2*margin, CHK_size]);
         offset = offset + 20;
     end
 end    
+% blank out missings
+value_objs(value_objs==0)=[];
+string_objs(string_objs==0)=[];
+drop_objs(drop_objs==0)=[];
 
-uicontrol(fig, 'Style','pushbutton','String','Run', ...
+% add buttons
+uicontrol(fig, 'Style','pushbutton','String','Run','Tag','RunBtn','Callback' ,@done ,...
           'Pos', [(width/2)+padding height-(height-10) ((width-2*margin)/2)-margin, BTN_size]) 
-uicontrol(fig, 'Style','pushbutton','String','Cancel', ...
+uicontrol(fig, 'Style','pushbutton','String','Cancel','Tag','CancelBtn','Callback' ,@done , ...
           'Pos', [margin height-(height-10) ((width-2*margin)/2)-padding, BTN_size])       
+
+% Wait for input
 set(fig,'Visible','on');
-a=2; % debug point =)
+uiwait(fig);
+
+% Parse input
+if ishghandle(fig)
+    if strcmp(get(fig,'UserData'),'OK'),
+        if ~exist('resps','var')
+            resps=struct();
+        end
+        for j=1:numel(string_objs)
+            resps.(get(string_objs(j),'Tag')) = get(string_objs(j),'String');
+        end
+        for j=1:numel(drop_objs)
+            resps.(get(drop_objs(j),'Tag')) =  getCurrentPopupString(drop_objs(j));
+        end
+        for j=1:numel(value_objs)
+            resps.(get(value_objs(j),'Tag')) = get(value_objs(j),'Value');
+        end
+        delete(fig);
+    end
+else
+  resps=[];  %#ok<NASGU>
+end
+
+a=2; % debug point =).
+end
+
+function doneKeyPress(obj, evd) %#ok
+    switch(evd.Key)
+      case {'return'}
+        if ~strcmp(get(obj,'UserData'),'Cancel')
+          set(gcbf,'UserData','OK');
+          uiresume(gcbf);
+        else
+          delete(gcbf)
+        end
+      case 'escape'
+        delete(gcbf)
+    end
+end
+
+function done(obj, evd) %#ok
+    if ~strcmp(get(obj,'UserData'),'Cancel')
+      set(gcbf,'UserData','OK');
+      uiresume(gcbf);
+    else
+      delete(gcbf)
+    end
+end
+
+function str = getCurrentPopupString(hh)
+%# getCurrentPopupString returns the currently selected string in the popupmenu with handle hh
+
+%# could test input here
+if ~ishandle(hh) || strcmp(get(hh,'Type'),'popupmenu')
+error('getCurrentPopupString needs a handle to a popupmenu as input')
+end
+
+%# get the string - do it the readable way
+list = get(hh,'String');
+val = get(hh,'Value');
+if iscell(list)
+   str = list{val};
+else
+   str = list(val,:);
+end
+
+end
