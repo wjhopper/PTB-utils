4d095322bbea13f9fce4cdaf8e4222eab15b4fa3
whopper-cemnl <wjhopper510@gmail.com>
2016-04-04 15:21:47 -0400

    merged countdown timing fix from LB4L2 experiment

--- PsychUI/countdown.m
+++ PsychUI/countdown.m
@@ -1,10 +1,10 @@
 function countdown(text, start, speed, window, constants)
-    times = start:-1:1; 
-    wakeup = GetSecs;
+    times = start:-1:1;
+    wakeup = Screen('Flip', window);
     for i = times
         message = strjoin({text, num2str(i)}, '\n\n');
-        DrawFormattedText(window, message, 'center', 'center',[], constants.wrapat,[],[],1.5);
-        vbl = Screen('Flip', window, wakeup -(constants.ifi/2));
-        wakeup = WaitSecs('UntilTime', vbl + speed);
+        DrawFormattedText(window, message, 'center', 'center');
+        vbl = Screen('Flip', window, wakeup + (constants.ifi/2));
+        wakeup = WaitSecs('UntilTime', vbl + speed - constants.ifi);
     end
 end
